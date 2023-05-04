e73c988616796a065cb0b43ddd3db99fa00a5568
Joshua Welch <oneadvent@joshuawelch.me>
2023-05-03 20:34:33 -0700

    make a secret admin page. will store as a cookie

--- admin.php
+++ admin.php
@@ -1,5 +1,27 @@
 <?php 
-$page_name="Admin";
-require('header.php'); ?>
-Admin page
-<?php require('footer.php'); ?>
+$page_name = "Admin";
+require('header.php');
+
+if (isset($_POST["password"])) {
+  //set cookie
+  setcookie("password", $_POST["password"]);
+} else if (isset($_COOKIE["password"])) {
+  //check cookie
+  $_POST["password"] = $_COOKIE["password"];
+}
+
+//check password = "secretPassWORD" from POST
+if (isset($_POST["password"]) && $_POST["password"] == "secretPassWORD") {
+  //show admin page
+  echo "Welcome Admin";
+} else {
+  //show password form
+  echo "Please Enter Password:";
+  echo "<form action='admin.php' method='post'>";
+  echo "<input type='password' name='password'>";
+  echo "<input type='submit'>";
+  echo "</form>";
+}
+
+require('footer.php');
+?>
--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php 
 $page_name="Home";
 require('header.php'); ?>
-home page.
+List of results:
+<?php
+$sql = "SELECT * FROM golfer_data";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  echo "<table class='table table-striped'><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Score</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["id"]."</td><td>".$row["first_name"]."</td><td>".$row["last_name"]."</td><td>".$row["score"]."</td></tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+?>
+
 <?php require('footer.php'); ?>
