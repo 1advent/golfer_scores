868cede057f17de79400ee29a746b4ff0b1ceac9
Joshua Welch <oneadvent@joshuawelch.me>
2023-05-03 19:34:55 -0700

    connect to db

---
+++ database_stuffs.php
@@ -0,0 +1,30 @@
+<?php
+// Load environment variables
+$dotenv = fopen('.env', 'r');
+while (!feof($dotenv)) {
+  $line = trim(fgets($dotenv));
+  if ($line && strpos($line, '=') !== false) {
+    list($key, $value) = explode('=', $line, 2);
+    $_ENV[$key] = $value;
+  }
+}
+fclose($dotenv);
+
+// Set up database connection
+$host = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$database = $_ENV['DB_DATABASE'];
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// rest of your code...
+
+?>
