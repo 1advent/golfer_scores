07bc02f46919187adacc1e46f1cfa46bb7a6fab2
Joshua Welch <oneadvent@joshuawelch.me>
2023-05-04 18:05:11 -0700

    reverted from score to golferScore

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ $page_name="Home";
 require('header.php'); ?>
 List of results:
 <?php
-$sql = "SELECT * FROM golfer_data order by score desc";
+$sql = "SELECT * FROM golfer_data order by golferScore desc";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   echo "<table class='table table-striped'><tr><th>First Name</th><th>Score</th></tr>";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<tr><td>".$row["players"]."</td><td>".$row["score"]."</td></tr>";
+    echo "<tr><td>".$row["players"]."</td><td>".$row["golferScore"]."</td></tr>";
   }
   echo "</table>";
 } else {
---
+++ update_tables.php
@@ -0,0 +1,3 @@
+<?php
+require('database_stuffs.php');
+?>
